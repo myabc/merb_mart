8388fe58541d56708c0b85fcc0e940d7946c9179
Alex Coles <alexbcoles@mac.com>
2008-05-08 04:06:13 +0200

    Removing specs for deleted controllers, helpers.

--- spec/controllers/admin/sections_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), "../..", 'spec_helper.rb')
-
-describe Admin::Sections, "index action" do
-  before(:each) do
-    @controller = Admin::Sections.build(fake_request)
-    @controller.dispatch('index')
-  end
-end
--- spec/controllers/content_nodes_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
-
-describe ContentNodes, "index action" do
-  before(:each) do
-    dispatch_to(ContentNodes, :index)
-  end
-end
