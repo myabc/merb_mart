82d5d35a57e3408aa5ae1857e93355bca7b9648a
Alex Coles <alexbcoles@mac.com>
2008-05-08 02:32:49 +0200

    Added dm-aggregates (for User class).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 require 'digest/sha1'
 require "date"
+require "dm-aggregates"
 require "dm-validations"
 begin
   require File.join(File.dirname(__FILE__), '..', '..', "lib", "authenticated_system", "authenticated_dependencies")
